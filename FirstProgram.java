ba880a5bc19d096b3886a0f7f6cd9e81d5926eb7
Rishant Rokaha <53159963+Rixant@users.noreply.github.com>
2019-10-06 14:05:10 -0400

    Update FirstProgram.java

--- FirstProgram.java
+++ FirstProgram.java
@@ -2,12 +2,11 @@ import java.util.*;
 
 public class FirstProgram{
 public static void main(String[] args){
-System.out.println("Enter two whole numbers spaceseparated:");
+System.out.println("Enter two whole numbers separated by space:");
 int n1, n2;
-Scanner keyboard=new Scanner(System.in);
-n1=keyboard.nextInt();
-n2=keyboard.nextInt();
-System.out.println("Result is:");
-System.out.println(n1+n2);
+Scanner input=new Scanner(System.in);
+n1=input.nextInt();
+n2=input.nextInt();
+System.out.println("The sum of two numbers is="+ n1+n2);
 }
 }
