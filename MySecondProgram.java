193b8831c28608ad6c94719eb37739bd28f7ed7a
Rishant Rokaha <53159963+Rixant@users.noreply.github.com>
2019-10-06 21:49:20 -0400

    Update MySecondProgram.java

--- MySecondProgram.java
+++ MySecondProgram.java
@@ -4,16 +4,17 @@ public class MySecondProgram
 {
 public static void main(String[] args)
 {
-System.out.println("Enter two whole number separated with space:");
 int n1, n2;
 Scanner input=new Scanner(System.in);
+System.out.println("Enter biggest number= ");
 n1=input.nextInt();
+System.out.println("Enter smallest number= ");
 n2=input.nextInt();
-System.out.println("Sum is: "+(n1+n2));
-System.out.println("Difference is:"+ math.abs(a-b));
-System.out.println("Product is:"+a*b);
-System.out.println("Quotient is:"+n1/n2);
-System.out.println("Remainder is:"+n1%n2);
+System.out.println("Sum is= "+(n1+n2));
+System.out.println("Difference is= "+(n1-n2));
+System.out.println("Product is= "+(n1*n2));
+System.out.println("Quotient is= "+(n1/n2));
+System.out.println("Remainder is= "+(n1%n2));
 
 }
 }
